a81138ea7a72e3d2af499a2371eee7bc8582db30
Ricky Zhang Laptop <ricky.zhangNYC@gmail.com>
2018-04-02 16:29:11 -0400

    Editted for correctness

--- anagram.py
+++ anagram.py
@@ -9,16 +9,16 @@ s = "rat", t = "car", return false.
 def anagram(str1,str2):
     #Using a list to keep record of seen letters
     str1 = list(str1)
-    for i in range(len(str1)):
-        for j in range(len(str2)):
-            if str2[j] in str1:
-                str1.remove(str2[j])
-    if not str1:
-        return True
-    else:
-        return False
+    for i in range(len(str2)):
+        if str2[i] in str1:
+            str1.remove(str2[i])
+        else:
+            return False
+    return True
 
-str1 = 'anagram'
-str2 = 'margana'
+    #
+
+str1 = 'anagram hha'
+str2 = 'margana haha'
 
 print(anagram(str1,str2))
